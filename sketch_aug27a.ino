826675c5eee4f5e8b27dccc60e4ca2e9a7d6f297
unknown <jwjang0722@gmail.com>
2025-09-10 14:54:31 +0900

    first commit

---
+++ sketch_aug27a.ino
@@ -0,0 +1,17 @@
+#define LED 27
+#define LDR 32
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(115200);
+}
+void loop() {
+  int value = analogRead(LDR);
+  Serial.println(value);
+  if (value < 1000) {
+    digitalWrite(LED, HIGH);   // 어두우면 켜짐
+  } else {
+    digitalWrite(LED, LOW);    // 밝으면 꺼짐
+  }
+  delay(500);
+}
